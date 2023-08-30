4ba563b5655fbb5d5f662325ca0c3cc6589c2b11
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-08-30 22:34:05 +0600

    responsive modal and details added

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@ const loadCategory = async () => {
     const li = document.createElement("li");
 
     li.innerText = newsCategory.category_name;
+    li.classList = "hover:text-indigo-500";
     li.setAttribute("onclick", `handleSearch('${newsCategory.category_id}')`);
 
     category_container.appendChild(li);
@@ -21,7 +22,7 @@ const loadCategory = async () => {
 
 loadCategory();
 
-const handleSearch = async (searchId='08') => {
+const handleSearch = async (searchId = "08") => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/news/category/${searchId}`
   );
@@ -43,7 +44,7 @@ const showNewsCard = (newsArr) => {
   const cards_container = document.getElementById("cards-container");
   console.log(newsArr[0]);
 
-  cards_container.innerHTML=``
+  cards_container.innerHTML = ``;
 
   newsArr.forEach((news) => {
     const div = document.createElement("div");
@@ -52,7 +53,7 @@ const showNewsCard = (newsArr) => {
       "max-w-md bg-white border border-gray-200 rounded-lg shadow";
 
     div.innerHTML = `
-          <a >
+          <a>
             <img
               class="rounded-t-lg"
               src="${news.image_url}"
@@ -62,17 +63,17 @@ const showNewsCard = (newsArr) => {
           <div class="p-5">
             <a >
               <h5
-                class="mb-2 text-2xl font-bold tracking-tight text-gray-900 "
+                class="mb-2  text-2xl font-bold tracking-tight text-gray-900 "
               >
-                ${news.title}
+                ${news.title.slice(0, 30)}
               </h5>
             </a>
             <p class="mb-3 font-normal text-gray-700 ">
             ${news.details.slice(0, 70)} ...
             </p>
-            <a
-              
-              class="inline-flex cursor-pointer items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300  "
+            <button
+            onclick="handleModal('${news._id}')"
+              class="inline-flex cursor-pointer items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800"
             >
               Read more
               <svg
@@ -90,7 +91,7 @@ const showNewsCard = (newsArr) => {
                   d="M1 5h12m0 0L9 1m4 4L9 9"
                 />
               </svg>
-            </a>
+            </button>
           </div>
         
         
@@ -98,4 +99,22 @@ const showNewsCard = (newsArr) => {
 
     cards_container.appendChild(div);
   });
+};
+
+const handleModal = async (searchId) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/${searchId}`
+  );
+
+  const data = await res.json();
+
+  const news = data.data[0];
+
+  document.getElementById("news-title").innerText = news.title;
+
+  document.getElementById("news-img").setAttribute("src", `${news.image_url}`);
+
+  document.getElementById("news-desc").innerText = news.details;
+
+  modalId.showModal();
 };
